1f3302de01c3eb511f253b75f02ccf0c502f43c2
ekenecf <ekens2u@gmail.com>
2023-03-07 12:29:37 +0100

    Update Notification, Text and and Uploadjs files

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -2,7 +2,6 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
 import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
 
 function Notification(prop) {
   const notify = () => {
